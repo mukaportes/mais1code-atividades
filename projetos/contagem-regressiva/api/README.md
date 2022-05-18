4e4ecff55c95325580f3905d60739ead03b3f239
mukaportes <mport.dev@gmail.com>
2022-05-17 21:14:18 -0300

    Update projeto contagem

---
+++ projetos/contagem-regressiva/api/README.md
@@ -0,0 +1,6 @@
+# Rodando a API
+
+1. Baixar o NodeJS na maquina
+2. Ir até a pasta /api (cd /seu/caminho/mais1code-atividades/projetos/contagem-regressiva/api)
+3. Executar npm install no terminal
+4. Executar node ./src/index.js
--- projetos/contagem-regressiva/api/src/index.js
+++ projetos/contagem-regressiva/api/src/index.js
@@ -1,20 +1,23 @@
+// pacote Express https://www.npmjs.com/package/express
 const express = require('express');
 
-// cria novo app express
+// cria novo app express // cria um novo objeto express ao chamar express()
 const app = express();
 
 // cria uma rota para o endpoint /usuario no app
+// localhost 127.0.0.1:PORTA --> basePath (caminho base)
 app.post(
-  '/usuario',
-  (req, res) => {
-    const formulario = req.body;
+  '/usuario', // quero criar uma rota em: http://localhost:3000/usuario
+  (request, response) => {
+    const formulario = request;
 
-    console.log('Formulario: ', formulario);
+    console.log('Requisição: ', request);
+    console.log('Resposta: ', response);
 
-    res.status(200).json({ message: 'Usuário criado com sucesso' });
+    response.status(200).json({ message: 'Usuário criado com sucesso' });
   },
 );
 
 // inicia servidor na porta 3000
 app.listen(3000);
-console.log('App is litening on port 3000');
+console.log('Api ouvindo na porta 3000');
