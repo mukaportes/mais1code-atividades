63bf52359eab4460859dd56125b7e10b1d63b4e9
mukaportes <mport.dev@gmail.com>
2022-05-26 19:50:04 -0300

    Add aula 16-05

---
+++ aulas/nodejs/aula-26-05-2022/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const adicionarLinhasCallback = () => {
+  // Insere primeira nova linha
+  fs.appendFile('arquivo.txt', '\nnovaLinha', (err, data) => {
+    if (err) throw new Error(err);
+
+    // Lê arquivo
+    fs.readFile('arquivo.txt', (err, data) => {
+      if (err) throw new Error(err);
+
+      console.log('Dados do arquivo: ', data.toString());
+
+      // Insere segunda nova linha
+      fs.appendFile('arquivo.txt', '\nnovaLinha', (err, data) => {
+        if (err) throw new Error(err);
+
+        // Lê arquivo novamente
+        fs.readFile('arquivo.txt', (err, data) => {
+          if (err) throw new Error(err);
+
+          console.log('Dados do arquivo 2: ', data.toString());
+        });
+      });
+    });
+  });
+};
