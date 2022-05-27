4f25d3924b81f17522305c2606c3c7e6f88ba80e
mukaportes <mport.dev@gmail.com>
2022-05-26 21:01:35 -0300

    Update aula 26

--- aulas/nodejs/aula-26-05-2022/index.js
+++ aulas/nodejs/aula-26-05-2022/index.js
@@ -1,3 +1,5 @@
+// https://dev.to/viniciusersouza/explicando-promises-de-uma-forma-didtica-2pjk
+
 const fs = require('fs');
 
 const adicionarLinhasCallback = () => {
@@ -26,5 +28,81 @@ const adicionarLinhasCallback = () => {
   });
 };
 
-adicionarLinhasCallback();
-console.log('Após executar adicionarLinhasCallback()');
+// 3 motivos pra usar Promise:
+// 1 - quando voce precisar usar varias operações assincronas (promises) 
+// no mesmo bloco {}
+// 2 - quando voce executar codigo muito custoso (muitos dados, consumo alto de memoria)
+// deve-se colocar dentro de uma promise para o programa nao ficar esperando
+// 3 - quando voce precisar executar funcoes/processos em paralelo
+const funcaoPromise = (meuErro, meusDados) => new Promise((resolve, reject) => {
+  if (meuErro) funcaoPromise();
+  resolve(meusDados);
+});
+// const funcaoPromise = (meuErro, meusDados) => {
+//   if (meuErro) throw new Error(meuErro);
+//   return meusDados;
+// };
+
+// Executou a promise:
+// ESTADO: pending
+// ESTADO: resolved quando resolve()
+// ESTADO: rejected quando reject()
+// ESTADO: fullfiled quando resolved ou rejected
+let taCarregando = false;
+const resultadoPromise = funcaoPromise(undefined, 'Meu sucesso')
+  .then((resultado) => {
+    console.log('MEU RESULTADO', resultado);
+    taCarregando = false;
+  })
+  .catch((erro) => {
+    console.error('erro', erro);
+    taCarregando = false;
+  });
+taCarregando = true
+
+console.log('resultadoPromise', resultadoPromise);
+console.log('depois de exibir a promise ====================== ');
+
+setTimeout(() => {
+  console.log('resultadoPromise', resultadoPromise);
+}, 3000);
+// adicionarLinhasCallback();
+// console.log('Após executar adicionarLinhasCallback()');
+
+// Exemplos operações assincronas:
+// - conexao/operacao no banco de dados
+// - conexao HTTP
+// - APIs
+// - trabalhar com arquivos
+// - operaçoes em tempo real
+// - processar grandes quantidades de dados
+
+
+
+// Cadastrar um usuario no banco de dados
+// cadastrarUser(); 
+//    - validar usuario
+//    - formatar usuario
+//    - conectar com o banco de dados // assincrono
+//    - salvo no banco de dados // assincrono
+// enviaEmailDeConfirmação();
+//    - validar email
+//    - enviar email
+
+
+// EXECUTANDO DUAS COISAS AO MESMO TEMPO
+const primeiraFuncao = () => {
+  console.log(new Date().getTime());
+};
+const segundaFuncao = () => {
+  console.log(new Date().getTime());
+};
+
+primeiraFuncao();
+segundaFuncao();
+
+Promise.all([
+  funcaoPromise(),
+]).then((resultado) => {
+
+});
